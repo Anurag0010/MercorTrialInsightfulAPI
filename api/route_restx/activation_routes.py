4aa03cb1a551031bb770eea029d2728014610ba0
Anurag <arg29gupta@gmail.com>
2025-05-17 07:10:25 +0530

    push

--- api/route_restx/activation_routes.py
+++ api/route_restx/activation_routes.py
@@ -16,14 +16,8 @@ class ActivateEmployee(Resource):
         data = request.get_json(force=True)
         password = data.get('password', None)
         user_name = data.get('userName', None)
-        if user_name is None:
-            return {"message": "User name is required."}, 400
         if not password or len(password) < 6:
             return {"message": "Password is required and must be at least 6 characters."}, 400
-
-        # check if user name is already taken
-        if Employee.query.filter_by(username=user_name).first():
-            return {"message": "User name already exists."}, 400
         
         employee = Employee.query.filter_by(activation_token=token, is_active=False).first()
         if not employee or not employee.activation_token_expiry or employee.activation_token_expiry < datetime.utcnow():
